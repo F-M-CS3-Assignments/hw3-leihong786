1252f340c128bb5ec55701539cfcf16d8c79b3f7
leihong786 <leihong786@gmail.com>
2025-03-06 14:46:34 -0500

    first commit

---
+++ NasaLaunchAnalysis.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+#include "TimeCode.h"  // Custom class for handling time operations
+
+unsigned int i;  // Global variable (not really necessary in this context)
