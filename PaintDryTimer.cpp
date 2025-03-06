ef2e4710e867e8768b3994baffd15147a99b392e
leihong786 <leihong786@gmail.com>
2025-03-06 14:59:57 -0500

    asdas

---
+++ PaintDryTimer.cpp
@@ -0,0 +1,13 @@
+#include <ctime> // for time(0)
+#include <iostream> // for cin and cout
+#include <cmath> // for M_PI and others
+#include <vector> // for vectors
+#include <cstdlib> // for random
+#include <cassert> // for assert in the tests() function
+#include "TimeCode.h" // for timecode's 
+#include <cstring> // for tolower
+
+using namespace std;
+
+unsigned int i;
+
